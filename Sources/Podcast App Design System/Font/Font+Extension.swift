21e7d3ab02564035c68074f7fb3f32f4d38733db
alfiansyah <alfian.official.mail@gmail.com>
2024-07-13 18:31:39 +0700

    Typhography

--- Sources/Podcast App Design System/Font/Font+Extension.swift
+++ Sources/Podcast App Design System/Font/Font+Extension.swift
@@ -72,3 +72,37 @@ extension View {
     modifier(TitleStyle())
   }
 }
+
+public extension Font {
+  struct Display {
+    public static let xs = notoSans(.bold, size: 28)
+    public static let s = notoSans(.bold, size: 32)
+    public static let m = notoSans(.bold, size: 36)
+    public static let l = notoSans(.bold, size: 40)
+    public static let xl = notoSans(.bold, size: 44)
+  }
+  
+  struct Heading {
+    public static let xs = notoSans(.semiBold, size: 20)
+    public static let s = notoSans(.semiBold, size: 24)
+    public static let m = notoSans(.semiBold, size: 28)
+    public static let l = notoSans(.semiBold, size: 32)
+    public static let xl = notoSans(.semiBold, size: 36)
+  }
+  
+  struct Paragraph {
+    public static let xs = inter(.regular, size: 12)
+    public static let s = inter(.regular, size: 14)
+    public static let m = inter(.regular, size: 16)
+    public static let l = inter(.regular, size: 18)
+    public static let xl = inter(.regular, size: 20)
+  }
+  
+  struct Label {
+    public static let xs = notoSans(.regular, size: 10)
+    public static let s = notoSans(.regular, size: 12)
+    public static let m = notoSans(.regular, size: 14)
+    public static let l = notoSans(.regular, size: 16)
+    public static let xl = notoSans(.regular, size: 18)
+  }
+}
