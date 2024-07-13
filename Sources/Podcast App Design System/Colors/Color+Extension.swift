acada9ecca8d9100a4f1d8f74996b3e42063f7fe
alfiansyah <alfian.official.mail@gmail.com>
2024-07-13 15:43:47 +0700

    Update

--- Sources/Podcast App Design System/Colors/Color+Extension.swift
+++ Sources/Podcast App Design System/Colors/Color+Extension.swift
@@ -8,29 +8,29 @@
 import SwiftUI
 
 public extension Color {
-  static let foregroundMuted = Color("foreground/fg_muted", bundle: .module)
-  static let foregroundDefault = Color("foreground/fg_default", bundle: .module)
-  static let foregroundDisabled = Color("foreground/fg_disabled", bundle: .module)
-  static let foregroundEmphasis = Color("foreground/fg_emphasis", bundle: .module)
+  static let foregroundMuted = Color("fg_muted", bundle: .module)
+  static let foregroundDefault = Color("fg_default", bundle: .module)
+  static let foregroundDisabled = Color("fg_disabled", bundle: .module)
+  static let foregroundEmphasis = Color("fg_emphasis", bundle: .module)
 }
 
 public extension Color {
-  static let backgroundDefault = Color("background/bg_default", bundle: .module)
-  static let backgroundEmphasis = Color("background/bg_emphasis", bundle: .module)
-  static let backgroundMuted = Color("background/bg_muted", bundle: .module)
+  static let backgroundDefault = Color("bg_default", bundle: .module)
+  static let backgroundEmphasis = Color("bg_emphasis", bundle: .module)
+  static let backgroundMuted = Color("bg_muted", bundle: .module)
 }
 
 public extension Color {
-  static let defaultAccentEmphasis = Color("default/accent_emphasis", bundle: .module)
-  static let defaultAccentMuted = Color("default/accent_muted", bundle: .module)
-  static let defaultDangerEmphasis = Color("default/danger_emphasis", bundle: .module)
-  static let defaultDangerMuted = Color("default/danger_muted", bundle: .module)
-  static let defaultInfoEmphasis = Color("default/info_emphasis", bundle: .module)
-  static let defaultInfoMuted = Color("default/info_muted", bundle: .module)
-  static let defaultSuccessEmphasis = Color("default/success_emphasis", bundle: .module)
-  static let defaultSuccessMuted = Color("default/success_muted", bundle: .module)
-  static let defaultWarningEmphasis = Color("default/warning_emphasis", bundle: .module)
-  static let defaultWarningMuted = Color("default/warning_muted", bundle: .module)
+  static let defaultAccentEmphasis = Color("accent_emphasis", bundle: .module)
+  static let defaultAccentMuted = Color("accent_muted", bundle: .module)
+  static let defaultDangerEmphasis = Color("danger_emphasis", bundle: .module)
+  static let defaultDangerMuted = Color("danger_muted", bundle: .module)
+  static let defaultInfoEmphasis = Color("info_emphasis", bundle: .module)
+  static let defaultInfoMuted = Color("info_muted", bundle: .module)
+  static let defaultSuccessEmphasis = Color("success_emphasis", bundle: .module)
+  static let defaultSuccessMuted = Color("success_muted", bundle: .module)
+  static let defaultWarningEmphasis = Color("warning_emphasis", bundle: .module)
+  static let defaultWarningMuted = Color("warning_muted", bundle: .module)
 }
 
 public extension Color {
