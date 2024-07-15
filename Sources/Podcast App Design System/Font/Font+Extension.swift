2ef7443d18e1d92a3cc3b456b79053b02cdb7230
alfiansyah <alfian.official.mail@gmail.com>
2024-07-15 21:00:35 +0700

    Font scale

--- Sources/Podcast App Design System/Font/Font+Extension.swift
+++ Sources/Podcast App Design System/Font/Font+Extension.swift
@@ -19,34 +19,34 @@ extension Font {
 
 public extension Font {
   struct Display {
-    public static let xs = notoSans(.bold, size: 28)
-    public static let s = notoSans(.bold, size: 32)
-    public static let m = notoSans(.bold, size: 36)
-    public static let l = notoSans(.bold, size: 40)
-    public static let xl = notoSans(.bold, size: 44)
+    public static let xs = notoSans(.bold, size: UIFontMetrics.default.scaledValue(for: 28))
+    public static let s = notoSans(.bold, size: UIFontMetrics.default.scaledValue(for: 32))
+    public static let m = notoSans(.bold, size: UIFontMetrics.default.scaledValue(for: 36))
+    public static let l = notoSans(.bold, size: UIFontMetrics.default.scaledValue(for: 40))
+    public static let xl = notoSans(.bold, size: UIFontMetrics.default.scaledValue(for: 44))
   }
   
   struct Heading {
-    public static let xs = notoSans(.semiBold, size: 20)
-    public static let s = notoSans(.semiBold, size: 24)
-    public static let m = notoSans(.semiBold, size: 28)
-    public static let l = notoSans(.semiBold, size: 32)
-    public static let xl = notoSans(.semiBold, size: 36)
+    public static let xs = notoSans(.semiBold, size: UIFontMetrics.default.scaledValue(for: 20))
+    public static let s = notoSans(.semiBold, size: UIFontMetrics.default.scaledValue(for: 24))
+    public static let m = notoSans(.semiBold, size: UIFontMetrics.default.scaledValue(for: 28))
+    public static let l = notoSans(.semiBold, size: UIFontMetrics.default.scaledValue(for: 32))
+    public static let xl = notoSans(.semiBold, size: UIFontMetrics.default.scaledValue(for: 36))
   }
   
   struct Paragraph {
-    public static let xs = inter(.regular, size: 12)
-    public static let s = inter(.regular, size: 14)
-    public static let m = inter(.regular, size: 16)
-    public static let l = inter(.regular, size: 18)
-    public static let xl = inter(.regular, size: 20)
+    public static let xs = inter(.regular, size: UIFontMetrics.default.scaledValue(for: 12))
+    public static let s = inter(.regular, size: UIFontMetrics.default.scaledValue(for: 14))
+    public static let m = inter(.regular, size: UIFontMetrics.default.scaledValue(for: 16))
+    public static let l = inter(.regular, size: UIFontMetrics.default.scaledValue(for: 18))
+    public static let xl = inter(.regular, size: UIFontMetrics.default.scaledValue(for: 20))
   }
   
   struct Label {
-    public static let xs = notoSans(.medium, size: 10)
-    public static let s = notoSans(.medium, size: 12)
-    public static let m = notoSans(.medium, size: 14)
-    public static let l = notoSans(.medium, size: 16)
-    public static let xl = notoSans(.medium, size: 18)
+    public static let xs = notoSans(.medium, size: UIFontMetrics.default.scaledValue(for: 10))
+    public static let s = notoSans(.medium, size: UIFontMetrics.default.scaledValue(for: 12))
+    public static let m = notoSans(.medium, size: UIFontMetrics.default.scaledValue(for: 14))
+    public static let l = notoSans(.medium, size: UIFontMetrics.default.scaledValue(for: 16))
+    public static let xl = notoSans(.medium, size: UIFontMetrics.default.scaledValue(for: 18))
   }
 }
